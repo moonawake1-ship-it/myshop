346fe196c12cd19fe93429c742621f1b0e679e8b
軍一 <moonawake1@gmail.com>
2026-05-16 01:20:47 +0800

    fix package for render

--- server.js
+++ server.js
@@ -142,6 +142,8 @@ ${message}
 });
 
 // 啟動伺服器
-app.listen(3000, () => {
-    console.log('🚀 Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
 });
